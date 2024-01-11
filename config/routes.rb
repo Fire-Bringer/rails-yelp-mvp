8eb86094b0cf2811943febbd1c4740dfb86400d9
Rashad DuPaty <radupaty@gmail.com>
2024-01-11 13:01:40 +0900

    coded the New and Create functions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # See the index of all restaurants
   get 'restaurants', to: 'restaurants#index', as: 'restaurants'
 
+  # Create a new restaurant
+  get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+  post 'restaurants', to: 'restaurants#create'
+
   # See one restaurant
   get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
 
