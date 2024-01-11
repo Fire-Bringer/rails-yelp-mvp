7b7096df176facdffa0691005755ca55bdc0c57c
Rashad DuPaty <radupaty@gmail.com>
2024-01-11 12:42:06 +0900

    coded Show section with links to and from the Index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,4 +5,15 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
+
+private
+
+# Security Params
+def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :category)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # See the index of all restaurants
   get 'restaurants', to: 'restaurants#index', as: 'restaurants'
 
+  # See one restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
   # resources :restaurants
 end
