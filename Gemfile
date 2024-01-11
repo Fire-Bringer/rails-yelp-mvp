0e90ca3781a3afd53b2acc62a3089eda3dfa312f
Rashad DuPaty <radupaty@gmail.com>
2024-01-11 11:43:06 +0900

    seeded with faker to create 10 restaurants

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+# Faker
+gem 'faker'
+
 # Simple Form
 gem "simple_form"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Destroying all old restaurants'
+Restaurant.destroy_all
+
+puts 'Creating 10 new restaurants'
+10.times do
+  rest = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "#{rest.name} has been created"
+end
+
+puts 'Done!'
